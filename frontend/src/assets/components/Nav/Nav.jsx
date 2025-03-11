cb52d5e38220bbc2f670e4ae5b28eeb7f502825e
Abdulla Durrani <Abdullahkd@gmail.com>
2025-03-10 22:45:52 -0400

    styling nav bar, not happy with current itteration

--- frontend/src/assets/components/Nav/Nav.jsx
+++ frontend/src/assets/components/Nav/Nav.jsx
@@ -6,12 +6,14 @@ const Nav = () => {
       <div>
         <nav className='navbar'>
         <h2 id="site-title">akddev.co</h2>
+         {/* <div id='nav-button-container'> */}
           <ul id='nav-buttons'>
             <li id='line-item-1'><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">SEB 10/15</a></li>
+            <li id='line-item-2'><a href="#">About</a></li>
+            <li id='line-item-3'><a href="#">Contact</a></li>
+            <li id='line-item-4'><a href="#">SEB 10/15</a></li>
           </ul>
+        {/* </div>    */}
         </nav>
         {/* Add more content here */}
       </div>
@@ -19,3 +21,25 @@ const Nav = () => {
   }
 
   export default Nav
+
+
+
+
+  // const Nav = () => {
+  //   return (
+  //     <div>
+  //       <nav className='navbar'>
+  //       <h2 id="site-title">akddev.co</h2>
+  //        {/* <div id='nav-button-container'> */}
+  //         <ul id='nav-buttons'>
+  //           <li id='line-item-1'><a href="#">Home</a></li>
+  //           <li id='line-item-2'><a href="#">About</a></li>
+  //           <li id='line-item-3'><a href="#">Contact</a></li>
+  //           <li id='line-item-4'><a href="#">SEB 10/15</a></li>
+  //         </ul>
+  //       {/* </div>    */}
+  //       </nav>
+  //       {/* Add more content here */}
+  //     </div>
+  //   );
+  // }
