2e00d6ec97acdec4c7ea209adccc374dd691df4d
Abdulla Durrani <Abdullahkd@gmail.com>
2025-03-12 22:20:05 -0400

    redid web deisgn, minamilist approach, chaged colors to base colors, will work from bottom up and add few colors once the deisgn layout is in place, working on body position next, picture and buttons for portfolio

--- frontend/src/assets/components/Nav/Nav.jsx
+++ frontend/src/assets/components/Nav/Nav.jsx
@@ -6,16 +6,13 @@ const Nav = () => {
       <div>
         <nav className='navbar'>
         <h2 id="site-title">akddev.co</h2>
-         {/* <div id='nav-button-container'> */}
           <ul id='nav-buttons'>
             <li id='line-item-1'><a href="#">Home</a></li>
             <li id='line-item-2'><a href="#">About</a></li>
             <li id='line-item-3'><a href="#">Contact</a></li>
-            <li id='line-item-4'><a href="#">SEB 10/15</a></li>
           </ul>
-        {/* </div>    */}
         </nav>
-        {/* Add more content here */}
+        <div id='navbar-line'></div>
       </div>
     );
   }
