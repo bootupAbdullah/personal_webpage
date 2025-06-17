634de7f772d6286d83c8240986128c79f514abc2
Abdullah Durrani <134801570+bootupAbdullah@users.noreply.github.com>
2025-06-17 15:17:52 -0400

    added links to projects, cookie clicker, movie watchlist

--- frontend/src/assets/components/Home/Home.jsx
+++ frontend/src/assets/components/Home/Home.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import './home.css'
+import './home.css';
 
 const Home = ({ setPage }) => {
   return (
     <div className='home-container-div'>
-       <div id='home-image-div'>
+      <div id='home-image-div'>
         <img id="home-image-src" src="src/assets/images/profile_pic.jpg" alt="Home" />
       </div>
       <div className='home-application-buttons'>
-      <button onClick={() => setPage("CookieClickerGame")}>Cookie Clicker Game</button>
-      <button onClick={() => setPage("MovieWatchList")}>Movie Wachlist App</button>
-      <button onClick={() => setPage("ApplicationTracker")}>Application Tracker</button>
-      <button onClick={() => setPage("InterviewShare")}>Interview Sharing<br />Platform</button>
+        <button
+          onClick={() => window.open('https://bootupabdullah.github.io/portfolio_project_1_cookie_clicker_chronicles/', '_blank', 'noopener,noreferrer')}
+        >
+          Cookie Clicker Game
+        </button>
+        <button
+          onClick={() => window.open('https://sprightly-naiad-897bbc.netlify.app', '_blank', 'noopener,noreferrer')}
+        >
+          Movie Watchlist App
+        </button>
+        <button onClick={() => setPage("ApplicationTracker")}>Application Tracker</button>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
-
-
